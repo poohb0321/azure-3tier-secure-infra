ebcac64407005cd2ed394e225044758faba6a5a9
Pooja B <pbandla0321@gmail.com>
2025-05-15 16:09:09 -0400

    commit changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
 terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name   = "rg-secure-infra"
     storage_account_name  = "secureterraformstate"
