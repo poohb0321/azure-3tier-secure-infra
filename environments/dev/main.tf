9eb35c95e5b6a8c5206ae94340f833b793ae17d0
Pooja B <pbandla0321@gmail.com>
2025-05-15 13:15:31 -0400

    commit the changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -20,10 +20,10 @@ provider "azurerm" {
 
 module "networking" {
   source           = "../../modules/networking"
-  vnet_name        = "dev-vnet"
-  location         = "East US"
-  resource_group   = "rg-dev-infra"
-  address_space    = ["10.1.0.0/16"]
+  vnet_name        = "secure-vnet"
+  location         = "eastus"
+  resource_group   = "rg-secure-infra"
+  address_space    = ["10.0.0.0/16"]
   subnet_names     = ["web-tier", "app-tier"]
-  subnet_prefixes  = ["10.1.1.0/24", "10.1.2.0/24"]
+  subnet_prefixes  = ["10.0.1.0/24", "10.0.2.0/24"]
 }
