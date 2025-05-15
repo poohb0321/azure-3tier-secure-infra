2bf887b00c2b78071bfce30b8dc4e333ea57e153
Pooja B <pbandla0321@gmail.com>
2025-05-15 18:00:58 -0400

    commit the changes for environments/dev/main.tf file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
 
 terraform {
   required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -19,11 +20,16 @@ terraform {
   }
 }
 
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-dev-infra"
+  location = "East US"
+}
+
 module "networking" {
   source          = "../../modules/networking"
   vnet_name       = "dev-vnet"
-  location        = "East US"
-  resource_group  = "rg-dev-infra"
+  location        = azurerm_resource_group.dev.location
+  resource_group  = azurerm_resource_group.dev.name
   address_space   = ["10.1.0.0/16"]
   subnet_names    = ["web-tier", "app-tier", "appgw-subnet"]
   subnet_prefixes = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
@@ -31,8 +37,8 @@ module "networking" {
 
 resource "azurerm_public_ip" "appgw" {
   name                = "appgw-public-ip"
-  location            = "East US"
-  resource_group_name = "rg-dev-infra"
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -40,28 +46,24 @@ resource "azurerm_public_ip" "appgw" {
 module "waf" {
   source         = "../../modules/waf"
   appgw_name     = "appgw-dev"
-  location       = "East US"
-  resource_group = "rg-dev-infra"
-
-  # Get the 3rd subnet from networking module (appgw-subnet)
+  location       = azurerm_resource_group.dev.location
+  resource_group = azurerm_resource_group.dev.name
   subnet_id      = module.networking.subnet_ids[2]
-
-  # Use the public IP just created
   public_ip_id   = azurerm_public_ip.appgw.id
 }
 
 module "sentinel" {
   source         = "../../modules/sentinel"
   workspace_name = "law-dev"
-  location       = "East US"
-  resource_group = "rg-dev-infra"
+  location       = azurerm_resource_group.dev.location
+  resource_group = azurerm_resource_group.dev.name
 }
 
 module "keyvault" {
-  source         = "../../modules/keyvault"
-  name           = "kv-dev"
-  location       = "East US"
-  resource_group = "rg-dev-infra"
-  tenant_id      = "3547f3ce-b3af-40c6-bb98-3a1d7ee844bd"   
-  admin_object_id = "36563b85-7113-4da4-9b15-b44a34595c5c" 
+  source          = "../../modules/keyvault"
+  name            = "kv-dev"
+  location        = azurerm_resource_group.dev.location
+  resource_group  = azurerm_resource_group.dev.name
+  tenant_id       = "3547f3ce-b3af-40c6-bb98-3a1d7ee844bd"
+  admin_object_id = "36563b85-7113-4da4-9b15-b44a34595c5c"
 }
