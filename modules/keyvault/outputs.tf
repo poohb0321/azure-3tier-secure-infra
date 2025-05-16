bf3c01b99b01ff20926bba0e579095698306be1e
Pooja B <pbandla0321@gmail.com>
2025-05-16 15:29:57 -0400

    changes made to modules keyvault and evn/dev

--- modules/keyvault/outputs.tf
+++ modules/keyvault/outputs.tf
@@ -6,3 +6,8 @@ output "key_vault_id" {
   description = "The ID of the Key Vault"
   value = azurerm_key_vault.main.id
 }
+
+output "key_vault_name" {
+  value       = azurerm_key_vault.main.name
+  description = "The name of the Key Vault"
+}
