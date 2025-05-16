b1938c2c577d5a4f88dc71429d188c68a4b464d0
Pooja B <pbandla0321@gmail.com>
2025-05-15 22:29:46 -0400

    ch

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.22.0"
+      version = ">= 3.83.0, < 4.0.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.3.0"
 }
 
 provider "azurerm" {
